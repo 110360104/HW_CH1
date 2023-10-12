5916a8ee77a8f80a059420756dd026fb7e3469fe
110360104 <jacky24588341@gmail.com>
2023-10-12 08:04:49 +0800

    HW_CH1

---
+++ src/Page_10.java
@@ -0,0 +1,9 @@
+
+public class Page_10 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("歡迎用Java!");
+		System.out.println("開始使用Java吧!");
+	}
+}
---
+++ src/Page_9.java
@@ -0,0 +1,9 @@
+
+public class Page_9 
+{
+	public static void main(String[]args)
+	{
+		System.out.print("歡迎用Java!");
+		System.out.print("開始使用Java吧!");
+	}
+}
